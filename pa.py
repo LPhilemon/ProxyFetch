519f09d7699a42b8e4b56ce93909cf7cbd787e26
Philemon <72923339+lphil-coder@users.noreply.github.com>
2023-05-10 22:59:38 +0300

    [Setting up rss feed]

--- pa.py
+++ pa.py
@@ -1,7 +1,31 @@
+import feedparser
+
 from flask import Flask
+
+BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
 
 app = Flask(__name__)
 
+# @app.route("/")
+# def ():
+#     return"<p><a href="">PA</a></p>"
+
+
+feed = feedparser.parse(BBC_FEED)
+first_article = feed['entries'][0]
+return """<html>
+<body>
+<h1> BBC Headlines </h1>
+<b>{0}</b> <br/>
+<i>{1}</i> <br/>
+<p>{2}</p> <br/>
+</body>
+</html>""".format(first_article.get("title"), first_article.
+get("published"), first_article.get("summary"))
+
 @app.route("/")
-def ph_ar():
-    return"<p><a href="">PA</a></p>"
+def get_ph_ar_news():
+    return "no news is good news"
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
