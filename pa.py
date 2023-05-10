6c5a9710763331c17c253d953755c48cdc8c09cc
Philemon <72923339+lphil-coder@users.noreply.github.com>
2023-05-10 23:04:29 +0300

    [Indexed format for title and other specifics]

--- pa.py
+++ pa.py
@@ -11,21 +11,21 @@ app = Flask(__name__)
 #     return"<p><a href="">PA</a></p>"
 
 
-feed = feedparser.parse(BBC_FEED)
-first_article = feed['entries'][0]
-return """<html>
-<body>
-<h1> BBC Headlines </h1>
-<b>{0}</b> <br/>
-<i>{1}</i> <br/>
-<p>{2}</p> <br/>
-</body>
-</html>""".format(first_article.get("title"), first_article.
-get("published"), first_article.get("summary"))
-
 @app.route("/")
 def get_ph_ar_news():
-    return "no news is good news"
+    feed = feedparser.parse(BBC_FEED)
+    first_article = feed['entries'][0]
+    return """<html>
+    <body>
+    <h1> BBC Headlines </h1>
+    <b>{0}</b> <br/>
+    <i>{1}</i> <br/>
+    <p>{2}</p> <br/>
+    </body>
+    <   /html>""".format(first_article.get("title"), first_article.
+                         get("published"), first_article.get("summary"))
+
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
