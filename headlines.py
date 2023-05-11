558e2bf693781aebc88875b31a664e9d051b580f
Philemon <72923339+lphil-coder@users.noreply.github.com>
2023-05-11 10:43:28 +0300

    [Rendering template rather than innerhtml]

---
+++ headlines.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from flask import Flask
+
+app = Flask(__name__)
+@app.route("/")
+@app.route("/<publication>")
+def get_news(publication="bbc"):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    first_article = feed['entries'][0]
+    return render_template("home.html")
--- pa.py
+++ pa.py
@@ -1,6 +1,7 @@
 import feedparser
 
 from flask import Flask
+from flask import render_template
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 'cnn': 'http://rss.cnn.com/rss/edition.rss',
@@ -18,18 +19,10 @@ app = Flask(__name__)
 @app.route("/<publication>")
 
 
-def get_ph_ar_news(publication):
+def get_ph_ar_news(publication = 'bbc'):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
-    return """<html>
-    <body>
-    <h1> BBC Headlines </h1>
-    <b>{0}</b> <br/>
-    <i>{1}</i> <br/>
-    <p>{2}</p> <br/>
-    </body>
-    </html>""".format(first_article.get("title"), first_article.
-                         get("published"), first_article.get("summary"))
+    return render_template("home.html")
 
 
 
