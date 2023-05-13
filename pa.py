2f5fffbab5d1423e9b05e38877e1aa181194336a
Philemon <72923339+lphil-coder@users.noreply.github.com>
2023-05-13 16:18:24 +0300

    [Used weather api]

--- pa.py
+++ pa.py
@@ -4,6 +4,9 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import json
+import urllib
+
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
@@ -15,10 +18,30 @@ app = Flask(__name__)
 # def ():
 #     return"<p><a href="">PA</a></p>"
 
+DEFAULTS = {'publication': 'bbc',
+            'city': 'Kampala,UG'}
+
 
 @app.route("/")
-@app.route("/<publication>")
-def get_ph_ar_news():
+# @app.route("/<publication>")
+def home():
+
+
+    # get customized headlines, based on user input or default
+    publication = request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_ph_ar_news(publication)
+# get customized weather based on user input or default
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+        weather = get_weather(city)
+    return render_template("home.html", articles=articles,
+                       weather=weather)
+
+
+def get_ph_ar_news(query):
     query = request.args.get("publication")
 
     if not query or query.lower() not in RSS_FEEDS:
@@ -26,7 +49,25 @@ def get_ph_ar_news():
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home.html", articles=feed['entries'])
+    return feed['entries']
+   
+   
+   
+def get_weather(query):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=ca23467df425c3d0219f6923521b579e"
+    query = urllib.parse.quote(query)
+    url = api_url.format(query)
+    data = urllib.request.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description":
+                   parsed["weather"][0]["description"],
+                   "temperature": parsed["main"]["temp"],
+                   "city": parsed["name"]
+                   }
+
+    return weather
 
 
 if __name__ == '__main__':
