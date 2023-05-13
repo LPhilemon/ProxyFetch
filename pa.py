5ddf39932554e55bfc6a9d7ae8fbcc5f239326fc
Philemon <72923339+lphil-coder@users.noreply.github.com>
2023-05-13 12:05:13 +0300

    [Using Get]

--- pa.py
+++ pa.py
@@ -2,11 +2,12 @@ import feedparser
 
 from flask import Flask
 from flask import render_template
+from flask import request
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
-'cnn': 'http://rss.cnn.com/rss/edition.rss',
-'fox': 'http://feeds.foxnews.com/foxnews/latest',
-'iol': 'http://www.iol.co.za/cmlink/1.640'}
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest',
+             'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 app = Flask(__name__)
 
@@ -17,12 +18,15 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/<publication>")
+def get_ph_ar_news():
+    query = request.args.get("publication")
 
-
-def get_ph_ar_news(publication = 'bbc'):
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "bbc"
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home.html", articles = feed['entries'])
-
+    return render_template("home.html", articles=feed['entries'])
 
 
 if __name__ == '__main__':
